24013a14ccf9645b76e079a1c97c82a22eee5389
GRAF505 <alex2200397@mail.ru>
2025-06-21 01:09:54 +1000

    Описание ваших изменений

--- frontend/my-app/src/store/store.ts
+++ frontend/my-app/src/store/store.ts
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5555';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3333';
 
 
 export interface Category {
